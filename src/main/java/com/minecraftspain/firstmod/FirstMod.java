c5961a585a1fefd65e967c17416dd420a3ea95f1
Carlos <carlosgq_@hotmail.com>
2023-01-26 23:12:37 +0100

    [UPDATE]

--- src/main/java/com/minecraftspain/firstmod/FirstMod.java
+++ src/main/java/com/minecraftspain/firstmod/FirstMod.java
@@ -37,12 +37,6 @@ public class FirstMod
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
-
-
 
     public FirstMod()
     {
--- src/main/java/com/minecraftspain/firstmod/init/ItemInit.java
+++ src/main/java/com/minecraftspain/firstmod/init/ItemInit.java
@@ -1,9 +1,21 @@
 package com.minecraftspain.firstmod.init;
 
 import com.minecraftspain.firstmod.FirstMod;
+import com.mojang.math.Vector3d;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -11,7 +23,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
 
 
-    //public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "tab_"+FirstMod.MODID);
+    public static final ModCreativeTab instance = new ModCreativeTab(0, "tabcreaciones");
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MODID);
 
@@ -19,6 +31,52 @@ public class ItemInit {
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
 
+    // Teleport
+    public static final RegistryObject<Item> TELEPORT_STAFF = ITEMS.register("teletransportador",
+            () -> new TeleportStaff(new Item.Properties().tab(instance)));
+
+
+
+
+    public static class TeleportStaff extends Item {
+        public TeleportStaff(Properties properties) {
+            super(properties);
+        }
+
+        @Override
+        public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+            System.out.println("Estamos pulsando el boton derecho");
+            BlockHitResult ray = getPlayerPOVHitResult(world, player, ClipContext.Fluid.NONE);
+            BlockPos lookPos = ray.getBlockPos().relative(ray.getDirection());
+            player.setPos(lookPos.getX(), lookPos.getY(), lookPos.getZ());
+
+            player.getCooldowns().addCooldown(this, 60);
+
+            player.fallDistance = 0F;
+            world.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDERMAN_TELEPORT, SoundSource.PLAYERS, 1.0F, 1.0F);
+
+
+
+            return super.use(world, player, hand);
+        }
+
+        protected static BlockHitResult getPlayerPOVHitResult(Level world, Player player, ClipContext.Fluid fluidMode) {
+            double range = 15;
+
+            float f = player.getXRot();
+            float f1 = player.getYRot();
+            Vec3 vector3d = player.getEyePosition(1.0F);
+            float f2 = Mth.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+            float f3 = Mth.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+            float f4 = -Mth.cos(-f * ((float)Math.PI / 180F));
+            float f5 = Mth.sin(-f * ((float)Math.PI / 180F));
+            float f6 = f3 * f4;
+            float f7 = f2 * f4;
+            Vec3 vector3d1 = vector3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
+            return world.clip(new ClipContext(vector3d, vector3d1, ClipContext.Block.OUTLINE, fluidMode, player));
+        }
+
+    }
 
 
 
